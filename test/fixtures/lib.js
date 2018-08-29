ab2bad2d0f3ba60d42dce62667b6a73ef86adad8
Dmitry Mikheev <dmikheev@users.noreply.github.com>
2018-08-29 20:06:34 +0300

    Fix tests

--- test/fixtures/lib.js
+++ test/fixtures/lib.js
@@ -2,5 +2,5 @@ import getUrls from 'get-urls';
 
 const text = 'Lorem ipsum dolor sit amet, //sindresorhus.com consectetuer adipiscing http://yeoman.io elit.';
 export function process() {
-    getUrls(text);
+	getUrls(text);
 }
